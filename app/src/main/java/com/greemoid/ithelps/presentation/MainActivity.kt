1d94f8d6bac8b188ab806de6ce654c66e1fe84eb
greemoid <greemoiddd@gmail.com>
2022-08-01 12:38:31 +0300

    Initial commit

---
+++ app/src/main/java/com/greemoid/ithelps/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.greemoid.ithelps.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greemoid.ithelps.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
