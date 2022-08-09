37d6698fe02d35098ca0455de072377b5f954f36
greemoid <greemoiddd@gmail.com>
2022-08-09 12:32:12 +0300

    added instruments fragment

--- app/src/main/java/com/greemoid/ithelps/presentation/instruments/InstrumentsFragment.kt
+++ app/src/main/java/com/greemoid/ithelps/presentation/instruments/InstrumentsFragment.kt
@@ -5,17 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.greemoid.ithelps.R
+import com.greemoid.ithelps.databinding.FragmentInstrumentsBinding
 
 
 class InstrumentsFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstrumentsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruments, container, false)
+        binding = FragmentInstrumentsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnMoodTracker.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_choiceMoodFragment)
+        }
+
+        binding.btnDiary.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_diaryFragment)
+        }
+
+        binding.btnMeditation.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_meditationFragment)
+        }
+
+        binding.btnAnswers.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_questionsAndAnswersFragment)
+        }
+
+        binding.btnControl.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_conditionControlFragment)
+        }
+
+        binding.btnAffirmations.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_affirmationsFragment)
+        }
+
+        binding.btnQuotes.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_quotesFragment)
+        }
+
+        binding.btnArticles.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_articlesFragment)
+        }
+
+        binding.btnBreathing.setOnClickListener {
+            findNavController().navigate(R.id.action_instrumentsFragment_to_breathingFragment)
+        }
     }
 
 }
